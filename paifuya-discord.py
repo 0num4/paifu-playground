2894e4d69b91dc49c04b4bda0c20c46353d81a59
0num4 <r.oonuma@matsuri-tech.com>
2023-11-15 09:10:57 +0900

    feat: 動いている

--- paifuya-discord.py
+++ paifuya-discord.py
@@ -11,41 +11,23 @@ url = "https://5-data.amae-koromo.com/api/v2/pl3/career_ranking/num_games?mode=2
 uradora_endpoint = "https://5-data.amae-koromo.com/api/v2/pl3/career_ranking/%E9%87%8C%E5%AE%9D%E7%8E%87_rev?mode=26"
 avg_rank_doma = "https://5-data.amae-koromo.com/api/v2/pl3/career_ranking/avg_rank?mode=24"
 
-# Custom headers
-# headers = {
-#     "authority": "5-data.amae-koromo.com",
-#     "accept": "*/*",
-#     "accept-language": "ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5",
-#     "origin": "https://ikeda.sapk.ch",
-#     "sec-ch-ua": '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
-#     "sec-ch-ua-mobile": "?0",
-#     "sec-ch-ua-platform": '"macOS"',
-#     "sec-fetch-dest": "empty",
-#     "sec-fetch-mode": "cors",
-#     "sec-fetch-site": "cross-site",
-#     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
-# }
-
-users = ["levena", "timpoにゃ", "電なのです", "0㌍", "高梨ミーシャ", "たくみんvtuber"]
-# Making a GET request to the API
-response = requests.get(uradora_endpoint)
-found_nicknames = []
-# Checking if the request was successful
-if response.status_code == 200:
-    # Printing the response content
-    data = response.json()
-
-    for player in data:
-        if player.get("nickname") in users:
-            found_nicknames.append(player.get("nickname"))
-else:
-    # エラーが出た場合は終わる
-    print("dataの取得に失敗しました")
-    exit()
-
-print(data)
+users = ["levena", "timpoにゃ", "電なのです", "0㌍", "高梨ミーシャ", "たくみんvtuber", "ぺいぺいちゃん"]
 
 
-# dataの中に該当のnicknameがあるかどうか
+def check_nicknames_in_endpoints(nicknames, endpoints):
+    found_nicknames = {}
+    for endpoint in endpoints:
+        response = requests.get(endpoint)
+        if response.status_code == 200:
+            data = response.json()
+            for player in data:
+                player_nickname = player.get("nickname")
+                if player_nickname in nicknames:
+                    if player_nickname not in found_nicknames:
+                        found_nicknames[player_nickname] = []
+                    found_nicknames[player_nickname].append(endpoint)
 
-data
+    return found_nicknames
+
+
+print(check_nicknames_in_endpoints(users, [url, uradora_endpoint, avg_rank_doma]))

9966fb93a97c7b2c6852e89f4521a0758599e0d7
0num4 <r.oonuma@matsuri-tech.com>
2023-11-23 22:48:44 +0900

    feat(majsoul): config.jsonの取得とreccomend_listの取得

--- login-majsoul-jp-npm/index.ts
+++ login-majsoul-jp-npm/index.ts
@@ -28,6 +28,48 @@ async function setup() {
   const json = (await passport.json()) as VersionResponse;
   console.log(json);
 }
+type ConfigResponse = {
+  ip: {
+    name: string;
+    region_urls: {
+      url: string;
+      ob_url: string;
+    }[];
+  }[];
+  goods_sheleve_id: string;
+  yo_service_url: string[];
+  yo_sdk_js: string;
+  jp_shop_js: string;
+  jp_shop_id: string;
+};
+async function config(version: string) {
+  const headers = {
+    headers: {
+      accept: "application/json, text/plain, /",
+      "content-type": "application/json;charset=UTF-8",
+      "sec-ch-ua":
+        '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"Windows"',
+      Referer: "https://game.mahjongsoul.com/",
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+    },
+    method: "GET",
+  };
+  const configRes = await fetch(`${JP_HOST}/v${version}/config.json`, headers);
+  const json = (await configRes.json()) as ConfigResponse;
+  const url = json.ip[0].region_urls[0].url;
+  const passport_url = json.yo_service_url[0];
+  console.log(passport_url);
+  console.log(url);
+  console.log(json);
+  const getWebSocketServerRes = await fetch(
+    url + "?service=ws-gateway&protocol=ws&ssl=true",
+    headers
+  );
+  const json2 = await getWebSocketServerRes.json();
+  console.log(json2);
+}
 
 async function main() {
   const passport = await fetch("https://passport.mahjongsoul.com/user/login", {
@@ -48,4 +90,5 @@ async function main() {
   console.log(json);
 }
 // main();
-setup();
+// setup();
+await config("0.10.302.w");

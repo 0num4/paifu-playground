27cc7a9fd3291073bc41357e8d71075da114d7ad
0num4 <r.oonuma@matsuri-tech.com>
2024-03-24 16:27:19 +0900

    feat: とりあえずperse入れたりなど

--- getDahaiSpeedFromDynamoDB.py
+++ getDahaiSpeedFromDynamoDB.py
@@ -1,5 +1,8 @@
 import boto3
 from boto3.dynamodb.conditions import Key
+import PaifuTypesPydanticWithInternal
+import json
+import decimal
 
 
 def get_hand_event_record_data(table, key_value):
@@ -13,8 +16,10 @@ def get_hand_event_record_data(table, key_value):
             hand_event_records = hand_record.get("handEventRecord", [])
 
             for event_record in hand_event_records:
-                event_data = event_record.get("data")
-                print(f"Event Data: {event_data}")
+                event_data_str: str = event_record.get("data")
+                if event_data_str is not None:
+                    event_data = json.loads(event_data_str)
+                    print(f"Json Event Data: {event_data}")
     else:
         print("No item found with the given key.")
 
@@ -25,7 +30,7 @@ table_name = "paifu_playground"
 table = dynamodb.Table(table_name)
 
 # 取得したいアイテムのキー値
-key_value = "cni8r269nc78kahoitb0"
+key_value = "cnsfcgu9nc7ajnvfo1l0"
 
 # 関数の呼び出し
 get_hand_event_record_data(table, key_value)

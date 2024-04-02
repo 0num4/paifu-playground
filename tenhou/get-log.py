faa570cf196213cb26cda502416986added81df5
0num4 <r.oonuma@matsuri-tech.com>
2024-04-02 12:26:00 +0900

    feat: 動いてない

---
+++ tenhou/get-log.py
@@ -0,0 +1,45 @@
+# https://gist.github.com/nullpos/d6a10e1f4b1f906d8b6d
+# そもそもhttp://e.mjv.jp/0/log/archived.cgi?"が死んでいるので、
+
+# -*- coding: utf-8 -*-
+import re
+
+# import urllib
+import urllib.request
+import gzip
+import os
+
+archive_url = "http://e.mjv.jp/0/log/archived.cgi?"
+plain_url = "http://e.mjv.jp/0/log/plainfiles.cgi?"
+
+
+def gz(filename):
+    f_in = open("./log/" + filename + ".xml", "rb")
+    f_out = gzip.open("./log/" + filename + ".mjlog", "wb")
+    f_out.writelines(f_in)
+    f_out.close()
+    f_in.close()
+    os.remove("./log/" + filename + ".xml")
+    return
+
+
+def download():
+    f_url = open("urls.txt", "r")
+    for row in f_url:
+        urlidand = re.sub(r".*log\=(.*)\n", r"\1", row)
+        urlid = re.sub(r"(.*)&.*", r"\1", urlidand)
+        f_in = open("./log/" + urlidand + ".xml", "w")
+        text = urllib.request.urlopen(archive_url + urlid).read()
+
+        if len(text) < 10:
+            text = urllib.request.urlopen(plain_url + urlid).read()
+        f_in.write(text)
+        f_in.close()
+        print(urlidand)
+        gz(urlidand)
+    f_url.close()
+    return
+
+
+if __name__ == "__main__":
+    download()

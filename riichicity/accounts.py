f211bc808183e83421eaf3dd7073d78edbac3d9d
0num4 <r.oonuma@matsuri-tech.com>
2024-04-06 14:46:00 +0900

    feat: createaccount: プロトタイプ

--- riichicity/accounts.py
+++ riichicity/accounts.py
@@ -19,6 +19,7 @@ def ping_riichi_city(headers: dict) -> Types.accountTypes.PingResponse:
     return res
 
 
+# adjustIdをもらうため？
 def users_get_last_login(headers: dict, adid: str) -> Types.accountTypes.UserGetLastLoginResponse:
     payload = {"adid": adid}  # emailloginから返ってくる
     userGetLastLoginRes = requests.post(
@@ -31,6 +32,9 @@ def users_get_last_login(headers: dict, adid: str) -> Types.accountTypes.UserGet
     return userGetLastLoginRes
 
 
+# ####### 新規登録系 ########
+
+
 # TODO: 検証
 def users_send_email_code(headers: dict, email: str, codeType: int = 0, lang: str = "ja") -> any:
     payload = {"email": email, "codeType": codeType, "lang": lang}
@@ -41,6 +45,22 @@ def users_send_email_code(headers: dict, email: str, codeType: int = 0, lang: st
     return res
 
 
+def users_email_verify(headers: dict, code: str, codeType: int = 0) -> any:
+    payload = {"code": code, "codeType": codeType}
+    # cookieが必須で、cookieでemail判別してそう
+    res = requests.post("https://alicdn.mahjong-jp.net/users/emailVerify", json=payload)
+    res = res.json()
+    json.dump(res, open("users_email_verify.json", "w"))
+    print(res)
+    return res
+
+
+# ####### 新規登録系 ########
+
+# ログインはemailLoginが既に定義した
+
+
+# パスワード紛失
 def users_retrieve_account(headers: dict, content: str) -> Types.accountTypes.UsersRetrieveAccountResponse:
     payload = {"content": content}
     # ログインしてなくてもOK
@@ -52,12 +72,42 @@ def users_retrieve_account(headers: dict, content: str) -> Types.accountTypes.Us
     return usersRetrieveAccountRes
 
 
+# guestログインの流れ
+# 1. initSession
+# 2. users/tokenLogin
+
+
+# TODO: 検証
+# response EmailLoginResponse
+def users_token_login(
+    headers: dict,
+    adjustId: str,
+    logCreate: bool = True,
+    orinToken: str = "",
+    tokenType: int = 1,
+    tokenValue: str = "",  # uuidv4
+) -> any:
+    payload = {
+        "adjustId": adjustId,
+        "logCreate": logCreate,
+        "orinToken": orinToken,
+        "tokenType": tokenType,
+        "tokenValue": tokenValue,
+    }
+    usersTokenLoginRes = requests.post("https://alicdn.mahjong-jp.net/users/tokenLogin", json=payload, headers=headers)
+    usersTokenLoginRes = usersTokenLoginRes.json()
+    json.dump(usersTokenLoginRes, open("users_token_login.json", "w"))
+    usersTokenLoginRes = Types.accountTypes.UsersTokenLoginResponse(**usersTokenLoginRes, strict=True)
+    print(usersTokenLoginRes)
+    return usersTokenLoginRes
+
+
 def main():
     # get_res_bundle_data()
-    emailLoginRes = base.login_riichi_city()
-    headers = base.get_headers(emailLoginRes)
-    users_retrieve_account(headers, content="loq")
-
+    # emailLoginRes = base.login_riichi_city()
+    # headers = base.get_headers(emailLoginRes)
+    # users_retrieve_account(headers, content="loq")
+    users_email_verify({}, "560546", 0)
     print("end")
 
 

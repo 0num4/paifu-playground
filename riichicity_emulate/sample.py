1e4ebdd14f561990abee8a6ac986fddb4fd61671
0num4 <r.oonuma@matsuri-tech.com>
2024-06-04 19:27:37 +0900

    feat: とりあえずcsvの読み込みまで

--- riichicity_emulate/sample.py
+++ riichicity_emulate/sample.py
@@ -1,12 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import os
 
 
 def main():
-    x = np.linspace(0, 10, 100)
-    y = np.sin(x)
-    plt.plot(x, y)
-    plt.show()
+    readcsv()
+    # x = np.linspace(0, 10, 100)
+    # y = np.sin(x)
+    # plt.plot(x, y)
+    # plt.show()
+
+
+def readcsv():
+    # CSVファイルの存在確認
+    csv_file = "saka.csv"
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(current_dir, csv_file)
+    if not os.path.isfile(csv_path):
+        print(f"Error: {csv_file} does not exist.")
+        return
+
+    # CSVファイルの読み込み
+    df = pd.read_csv(csv_path)
+
+    # 最初の列が10のもののみにフィルター
+    filtered_df = df[df.iloc[:, 0] == 10]
+
+    # フィルター後のデータフレームを表示
+    print(filtered_df)
 
 
 if __name__ == "__main__":

7864c6cb5f18343d9b413fb6206e6047061bbf7c
0num4 <r.oonuma@matsuri-tech.com>
2024-06-05 19:54:49 +0900

    feat: yonmaでも回ることを確認

--- riichicity_emulate/streamlitsample.py
+++ riichicity_emulate/streamlitsample.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import sample
+import yonma_sample
 
 st.set_page_config(layout="wide")
 st.title("rcity 10dan saka Sampling")
@@ -9,11 +10,11 @@ num_dan = st.selectbox("Number of dan", [7, 8, 9, 10], index=3)
 num_max_pt = st.selectbox("Number of max points", [9000, 10000, 15000, 20000], index=3)
 if num_games is None or num_dan is None:
     st.stop()
-df = sample.readcsv(filter=num_dan)
+df = yonma_sample.readcsv(filter=num_dan)
 if df is not None:
     if st.button("再計算") or num_games is not None:
         col1, col2 = st.columns(2)
-        plt = sample.simulate_games(df, num_games=num_games, max_score=num_max_pt)
+        plt = yonma_sample.simulate_games(df, num_games=num_games, max_score=num_max_pt)
         with col1:
             st.pyplot(plt)
         with col2:

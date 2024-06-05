ee4c7c193e5c34b859987cf6197ea539a6162fdc
0num4 <r.oonuma@matsuri-tech.com>
2024-06-05 16:35:14 +0900

    feat: rename

--- riichicity_emulate/rand.py
+++ riichicity_emulate/rand.py
@@ -1,13 +1,13 @@
 import random
 
 
-def random_test(num_trials=1000):
+def random_test(num_trials=10000):
     count_1 = 0
     count_2 = 0
     count_3 = 0
 
     for _ in range(num_trials):
-        result = random.choices([1, 2, 3], weights=[35, 45, 20])[0]
+        result = random.choices([1, 2, 3], weights=[0.3636, 0.3322, 0.3042])[0]
         if result == 1:
             count_1 += 1
         elif result == 2:

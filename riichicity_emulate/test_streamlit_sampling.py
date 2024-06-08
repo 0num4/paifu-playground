b5f0b99b39994ce177676956edd3a44cab7d06b0
0num4 <r.oonuma@matsuri-tech.com>
2024-06-08 13:21:47 +0900

    feat: もう少しテストを書くなどした

--- riichicity_emulate/test_streamlit_sampling.py
+++ riichicity_emulate/test_streamlit_sampling.py
@@ -1,3 +1,7 @@
 import streamlit.testing.v1
 
 at = streamlit.testing.v1.AppTest.from_file("streamlit_sampling.py")
+at.run(timeout=10)
+select = at.selectbox[0]
+assert select.options == ["100", "500", "1000", "2000", "3000"]
+assert not at.exception

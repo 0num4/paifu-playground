8e8afee7ecd0bf4b801bd52d2592c2f6b97957f3
0num4 <r.oonuma@matsuri-tech.com>
2024-06-08 13:34:54 +0900

    feat: add comment

--- riichicity_emulate/test_streamlit_sampling.py
+++ riichicity_emulate/test_streamlit_sampling.py
@@ -4,4 +4,6 @@ at = streamlit.testing.v1.AppTest.from_file("streamlit_sampling.py")
 at.run(timeout=10)
 select = at.selectbox[0]
 assert select.options == ["100", "500", "1000", "2000", "3000"]
+assert at.dataframe[0].empty is False
+# at.pyplotは実装されてない
 assert not at.exception
